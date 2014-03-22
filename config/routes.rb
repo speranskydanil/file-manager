47f05a8e4e4b9bc4aa50edd20ce24fb18819a87e
Speransky Danil <speranskydanil@gmail.com>
2014-03-22 16:22:43 +0700

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 FileManager::Application.routes.draw do
-  get '/base_dir', to: 'application#base_dir'
+  get '/base_dir', to: 'file_manager#base_dir'
 
-  get  '/list/:dir',          to: 'application#list',     :constraints => { :dir => /[^\/]+/ }
-  get  '/read/:file',         to: 'application#read',     :constraints => { :file => /[^\/]+/ }
-  post '/move/:from/:to',     to: 'application#move',     :constraints => { :from => /[^\/]+/, :to => /[^\/]+/ }
-  post '/copy/:from/:to',     to: 'application#copy',     :constraints => { :from => /[^\/]+/, :to => /[^\/]+/ }
-  post '/remove/:path',       to: 'application#remove',   :constraints => { :path => /[^\/]+/ }
-  post '/rename/:path/:name', to: 'application#rename',   :constraints => { :path => /[^\/]+/, :name => /[^\/]+/ }
-  post '/zip/:path/:name',    to: 'application#zip',      :constraints => { :path => /[^\/]+/, :name => /[^\/]+/ }
-  post '/unzip/:file/:name',  to: 'application#unzip',    :constraints => { :file => /[^\/]+/, :name => /[^\/]+/ }
-  get  '/download/:file',     to: 'application#download', :constraints => { :file => /[^\/]+/ }
+  get  '/list/:dir',          to: 'file_manager#list',     constraints: { dir: /[^\/]+/ }
+  get  '/read/:file',         to: 'file_manager#read',     constraints: { file: /[^\/]+/ }
+  post '/move/:from/:to',     to: 'file_manager#move',     constraints: { from: /[^\/]+/, to: /[^\/]+/ }
+  post '/copy/:from/:to',     to: 'file_manager#copy',     constraints: { from: /[^\/]+/, to: /[^\/]+/ }
+  post '/remove/:path',       to: 'file_manager#remove',   constraints: { path: /[^\/]+/ }
+  post '/rename/:path/:name', to: 'file_manager#rename',   constraints: { path: /[^\/]+/, name: /[^\/]+/ }
+  post '/zip/:path/:name',    to: 'file_manager#zip',      constraints: { path: /[^\/]+/, name: /[^\/]+/ }
+  post '/unzip/:file/:name',  to: 'file_manager#unzip',    constraints: { file: /[^\/]+/, name: /[^\/]+/ }
+  get  '/download/:file',     to: 'file_manager#download', constraints: { file: /[^\/]+/ }
 
-  post '/mkdir/:path',        to: 'application#mkdir',    :constraints => { :path => /[^\/]+/ }
+  post '/mkdir/:path',        to: 'file_manager#mkdir',    constraints: { path: /[^\/]+/ }
 
   root 'application#root'
 end
